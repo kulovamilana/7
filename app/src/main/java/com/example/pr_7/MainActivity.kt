608db156131efd376f938a82ebe5cca0896e63ba
kulovamilana <milankakulova@gmail.com>
2023-09-30 10:00:42 +0300

    first commit

---
+++ app/src/main/java/com/example/pr_7/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.pr_7
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
